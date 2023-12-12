0c94df6e912a9e5646c25772af5e52128d42daf5
Tyler Baur <baur.tyler@gmail.com>
2023-12-12 14:49:04 -0600

    Add docs for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,9 @@ from typing import Any
 @pytest.fixture
 def markdown_fixture():
     def _method(
-        extensions: list[str] | str, extension_config: dict[str : dict[str, Any]]
+        extensions: list[str] | str, extension_config: dict[str , dict[str, Any]]
     ):
+        """Fixture to dynamically insert markdown extensions."""
         return markdown.Markdown(
             extensions=extensions, extension_configs=extension_config
         )
